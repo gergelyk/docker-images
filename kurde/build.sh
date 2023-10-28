3df5a65e788587442cdf31053e289559ab22d71e
Grzegorz Krason <grzegorz.krason@gmail.com>
2023-10-28 13:05:19 +0200

    add latest

--- kurde/build.sh
+++ kurde/build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 KURDE_VERSION=0.2.0
+docker build -t gergelyk/kurde:latest --build-arg KURDE_VERSION=$KURDE_VERSION .
 docker build -t gergelyk/kurde:$KURDE_VERSION --build-arg KURDE_VERSION=$KURDE_VERSION .
